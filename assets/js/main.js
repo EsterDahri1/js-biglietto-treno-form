f7c6b896b50ebee60d8730a7c59fd8ee8724e7d4
EsterDahri <esterdahri@gmail.com>
2023-08-24 17:55:55 +0200

    delete everything rom js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,11 @@
 /*Descrizione:
 Scrivere un programma che chieda all’utente:
 Il numero di chilometri da percorrere
-Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+Età del passeggero 
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 
 */ 
-
-//Chiedo nome dell'utente
-const userName = document.getElementById("myName");
-
-//Chiedo km da percorrere nell'input
-const userKm = Number(document.getElementById("myDistance"));
-
-//Chiedo età del passeggero
-
-//Enuncio prezzo pieno
-let fullPrice = 0.21 * userKm;
-
-//Output in console
-
-
-//If/else if/else 
 
