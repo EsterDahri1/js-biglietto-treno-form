5bed5cca91bd2973af3c80b2f35cd4d34bd5bc3d
EsterDahri <esterdahri@gmail.com>
2023-08-24 18:18:40 +0200

    add input and if/else structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,29 @@ va applicato uno sconto del 40% per gli over 65.
 
 */ 
 
+//Chiedo nome, km da percorrere, età del passeggero 
+document.getElementById('generate').addEventListener('click', function(){
+    const userName = document.getElementById('myName').value;
+    console.log(userName);
+    const userDistance = Number(document.getElementById('myDistance').value);
+    console.log(userDistance);
+    const userAge = document.getElementById('myAge').value;
+    console.log(userAge);
+})
+
+//Definisco il prezzo in base ai km
+let fullPrice = 0.21 * userDistance;
+console.log(fullPrice);
+
+//if/else if/else
+if (userAge === Minorenne) {
+    var total_1 = fullPrice - (fullPrice * 0.20);
+    total_1 = total_1.toFixed(2);
+    console.log(total_1);   
+} else if (userAge === Over65) {
+    var total_2 = fullPrice - (fullPrice * 0.40);
+    total_2 = total_2.toFixed(2);
+    console.log(total_2); 
+} else {
+    console.log(fullPrice);
+}
